95075541e8c8b9eb4709945d4398d2a45be6d21f
Netdrone2407 <netdrone24@gmail.com>
2024-12-02 02:06:23 +0530

    again change

--- components/mydb.js
+++ components/mydb.js
@@ -1,6 +1,6 @@
 //require('dotenv').config();
 const mongoose = require('mongoose');
-const uris="mongodb+srv://netdrone24:Vaibs%402004@cluster0.rpz4c.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0";
+const uris=process.env.MONGO_URI;
 const connectDB = async () => {
   try {
    // console.log("Mongo URI:", process.env.MONGO_URI); // Debug
